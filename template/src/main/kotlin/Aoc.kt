0bddc35bf7f3e43b44983d28d80f3f7764d21840
Tim Lenneryd <tim.lenneryd@cygni.se>
2022-11-30 20:33:11 +0100

    Update Template.

--- template/src/main/kotlin/Aoc.kt
+++ template/src/main/kotlin/Aoc.kt
@@ -1,7 +1,9 @@
 import java.io.File
+import kotlin.math.abs
+import kotlin.math.absoluteValue
 
 fun main() {
-    val input = File("input.txt").readLines().map { it.toInt() }
+    val input = File("input.txt").readLines().mapInput()
     val answer = when (System.getenv("part")) {
         "part2" -> solutionPart2(input)
         else -> solutionPart1(input)
@@ -9,8 +11,16 @@ fun main() {
     println(answer)
 }
 
-fun List<String>.mapInput() = map { it.toInt() }
+data class Crab(val pos: Int)
 
-fun solutionPart1(input: List<Int>) = input.sum()
+fun List<String>.mapInput() = this.first().split(",").map { Crab(it.toInt()) }
 
-fun solutionPart2(input: List<Int>) = input.reduce(Int::times)
+fun solutionPart1(input: List<Crab>) =
+    (0..input.maxOf { it.pos }).minOf { current -> input.sumOf { (it.pos - current).absoluteValue } }
+
+fun solutionPart2(input: List<Crab>) = (input.minOf { it.pos }..input.maxOf { it.pos }).map { range ->
+    // Sum cost based on number of steps required for specific crab.
+    input.sumOf { crab -> crab.pos numStepsTo range }
+}.minOf { it }
+
+infix fun Int.numStepsTo(other: Int) = (1..(abs(this - other))).sum()
--- template/src/test/kotlin/AocTest.kt
+++ template/src/test/kotlin/AocTest.kt
@@ -3,15 +3,15 @@ import kotlin.test.assertEquals
 
 internal class AocTest {
 
-    private val list = listOf("1337", "42")
+    private val list = listOf("16,1,2,0,4,2,7,1,2,14")
 
     @Test
     internal fun `solutionPart1 should return part1 result`() {
-        assertEquals(1379, solutionPart1(list.mapInput()))
+        assertEquals(37, solutionPart1(list.mapInput()))
     }
 
     @Test
     internal fun `solutionPart2 should return part2 result`() {
-        assertEquals(56154, solutionPart2(list.mapInput()))
+        assertEquals(168, solutionPart2(list.mapInput()))
     }
 }
